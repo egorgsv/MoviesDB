550420985f82d48e56d4fcd2459a04921c1256d3
Egor Gusev <egorspb3@gmail.com>
2019-12-09 19:48:59 +0300

    Add program

---
+++ MoviesDB/Tag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoviesDB
+{
+    public class Tag
+    {
+        public string tag { get; set; }
+        public string tagId { get; set; }
+        public HashSet<MovieTag> MovieTags { get; set; }
+
+        public Tag()
+        {
+            MovieTags = new HashSet<MovieTag>();
+        }
+    }
+}
